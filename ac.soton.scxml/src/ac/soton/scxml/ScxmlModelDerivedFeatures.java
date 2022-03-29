15dc6c31ef9376d720f439efb3fcf5503a8477a0
Bálint Radnai <radnai.balint@edu.bme.hu>
2022-03-30 01:46:13 +0200

    Add parent and child state getters

--- ac.soton.scxml/src/ac/soton/scxml/ScxmlModelDerivedFeatures.java
+++ ac.soton.scxml/src/ac/soton/scxml/ScxmlModelDerivedFeatures.java
@@ -7,68 +7,89 @@ import java.util.stream.Collectors;
 import org.eclipse.emf.ecore.EObject;
 
 public class ScxmlModelDerivedFeatures {
-	
+
 	public static boolean isParallel(EObject scxmlElement) {
 		return scxmlElement instanceof ScxmlParallelType;
 	}
-	
+
 	public static boolean isState(EObject scxmlElement) {
 		return scxmlElement instanceof ScxmlStateType;
 	}
-	
+
 	public static boolean isFinal(EObject scxmlElement) {
 		return scxmlElement instanceof ScxmlFinalType;
 	}
-	
+
 	public static boolean isStateNode(EObject scxmlElement) {
 		return scxmlElement instanceof ScxmlStateType
 				|| scxmlElement instanceof ScxmlParallelType
 				|| scxmlElement instanceof ScxmlFinalType;
 	}
-	
+
 	public static boolean isScxmlOrStateNode(EObject scxmlElement) {
-		return scxmlElement instanceof ScxmlScxmlType
-				|| isStateNode(scxmlElement);
+		return scxmlElement instanceof ScxmlScxmlType || isStateNode(scxmlElement);
 	}
-	
+
 	public static boolean isCompoundState(ScxmlStateType state) {
-		return state.eContents().stream().anyMatch(it ->
-			isStateNode(it)
-		);
+		return state.eContents().stream().anyMatch(it -> isStateNode(it));
 	}
 	
+	public static boolean isTransitionSource(EObject scxmlElement) {
+		return scxmlElement instanceof ScxmlStateType
+				|| scxmlElement instanceof ScxmlParallelType
+				|| scxmlElement instanceof ScxmlInitialType
+				|| scxmlElement instanceof ScxmlHistoryType;
+	}
+
 	public static List<EObject> getStateNodes(EObject scxmlElement) {
 		if (!isScxmlOrStateNode(scxmlElement)) {
+			throw new IllegalArgumentException("EObject " + scxmlElement + " is not an Scxml state node.");
+		}
+
+		return scxmlElement.eContents().stream().filter(it -> isStateNode(it)).collect(Collectors.toList());
+	}
+
+	public static EObject getFirstChildStateNode(EObject scxmlElement) {
+		if (!(scxmlElement instanceof ScxmlScxmlType
+				|| (scxmlElement instanceof ScxmlStateType && isCompoundState((ScxmlStateType) scxmlElement)))) {
 			throw new IllegalArgumentException(
-					"EObject " + scxmlElement + " is not an Scxml state node.");
+					"EObject " + scxmlElement + " cannot contain an initial state element or attribute.");
 		}
-		return scxmlElement.eContents().stream().filter(it ->
-			isStateNode(it)
-		).collect(Collectors.toList());
+
+		return scxmlElement.eContents().stream().filter(it -> isStateNode(it)).findFirst().get();
+	}
+
+	public static ScxmlStateType getParentState(EObject scxmlElement) {
+		var parent = scxmlElement.eContainer();
+		if (!(parent instanceof ScxmlStateType)) {
+			throw new IllegalArgumentException(
+					"The parent element of EObject " + scxmlElement + " is not an Scxml state: " + parent);
+		}
+
+		return (ScxmlStateType) parent;
 	}
 	
-	public static String getParentStateNodeId(ScxmlTransitionType scxmlTransition) {
+	public static EObject getTransitionSource(ScxmlTransitionType scxmlTransition) {
 		var parent = scxmlTransition.eContainer();
-		if (parent instanceof ScxmlStateType) {
-			var parentState = (ScxmlStateType)parent;
-			var id = parentState.getId();
-			return id;
+		if (!isTransitionSource(parent)) {
+			throw new IllegalArgumentException(
+					"The parent element of transition " + scxmlTransition
+					+ " is not a valid Scxml transition source type: " + parent);
 		}
-		else {
-			return null;	// TODO Must work for ScxmlScxmlType, ScxmlParallelType and ScxmlInitialType, too.
-		}
+
+		return parent;
 	}
-	
+
 	public static List<ScxmlTransitionType> getAllTransitions(ScxmlScxmlType scxmlRoot) {
 		List<ScxmlTransitionType> allTransitions = new ArrayList<ScxmlTransitionType>();
 		var iterator = scxmlRoot.eAllContents();
 		while (iterator.hasNext()) {
 			var element = iterator.next();
 			if (element instanceof ScxmlTransitionType) {
-				allTransitions.add((ScxmlTransitionType)element);
+				allTransitions.add((ScxmlTransitionType) element);
 			}
 		}
 		return allTransitions;
 	}
-	
+
 }
